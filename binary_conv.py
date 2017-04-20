253d42738eb46c6bdd3cae9bb4ced97aa87cc66e
iwoty <iwo.tyszkowski@gmail.com>
2017-04-20 19:53:22 +0200

    Update binary_conv.py

--- binary_conv.py
+++ binary_conv.py
@@ -5,6 +5,7 @@ if len(entry) > 2 :
 elif len(entry) < 2 :
     print("Enter number to convert and actual system (2 or 10)!")
 elif len(entry) == 2 :
+
     #BINARY TO DECIMAL
     if int(entry[1]) == 2 :
         entry = entry[0][::-1] #'extended slices' [begin:end:step] - leaving begin and end, -1 reverses a string, helps with for loop
@@ -12,14 +13,24 @@ elif len(entry) == 2 :
         for i in range(len(entry)) :
             x = int(entry[i])*pow(2,i)
             decimal += x #sum of powers of 2 multiplied by 0 or 1 from entry (reversed) list
-        print(decimal,"10") #OUTPUT OF BINARY TO DECIMAL
+        print("/----------------------------\ ")
+        print("|               ",decimal,"10 |") #OUTPUT OF BINARY TO DECIMAL
+        print("\----------------------------/")
 
     #DECIMAL TO BINARY
     elif int(entry[1]) == 10 :
         entry = int(entry[0]) #change entered number to int
-        while entry > 1 :
-            entry //= 2 #floor division
-            print(entry % 2,end="")
-        print("1 2") #1 to add last 1, because our loop is working till >1, 2 to output numeral system
+        binary = [entry] #first element of the list is entered number
+        while entry > 1 : #next floor divisions to list
+            entry //= 2
+            binary.append(entry)
+        for i in range(len(binary)) : #changing the list from floor divisions to binary numbers
+            binary[i] %= 2 # if there is modulus - there would be 1, if not - 0
+        binary.reverse() #reverse to get proper binary number
+        print("/----------------------------\ ")
+        for i in range(len(binary)) :
+            print(binary[i], end='') #print binary from list to one line
+        print("| 2 |") #with addition of numeral system
+        print("\----------------------------/")
     else : #if provided numeral system is not 2 or 10
         print("Enter only numeral system 2 or 10!")
